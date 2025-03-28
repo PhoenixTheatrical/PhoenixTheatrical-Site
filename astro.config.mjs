d218e015ae6c9c29f9ed54260f889f09d3381758
Bryce Bienz <bryce.bienz@phoenixtheatrical.com>
2025-03-27 21:03:46 -0400

    Brandmarking, Style Guidelines using SASS 7-1-1 rule
    bunch of work with Brandmarking and styles... currently stuck on pointing results from a map.get to another map.get

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
 });
